752fc692b391d8cb98d4c8097f6f7ecc5b3c1aba
EdxCSU <133186995+EdxCSU@users.noreply.github.com>
2023-06-19 11:00:48 +0500

    Update MainKtTest.kt

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -39,14 +39,47 @@ class NoteTest {
         assertEquals(0, updatedComments.size)
         println("Комментарии к заметке с ID ($noteId) после удаления комментария: $updatedComments")
 
+
+
         note.delete(noteId)
         assertNull(note.getById(noteId))
         val emptyNotes = note.get()
         assertEquals(0, emptyNotes.size)
         println("Заметки после удаления: $emptyNotes")
+
+        if (updatedComments.isNotEmpty()) {
+            note.restoreComment(noteId, updatedComments[0].id)
+            val restoredComments = note.getComments(noteId)
+            assertEquals(1, restoredComments.size)
+            assertFalse(restoredComments[0].deleted)
+            println("Восстановленный комментарий к заметке с ID ($noteId): ${restoredComments[0]}")
+        } else {
+            println("Нет комментариев для восстановления.")
+        }
     }
 
+    @Test
+    fun testGetId() {
+        val note = Note<String>()
 
+        val noteItem1 = NoteItem(title = "Заметка 1", content = "Содержимое заметки 1")
+        note.add(noteItem1)
+        val id1 = note.getId()
+        assertEquals(1, id1)
+        println("ID первой заметки: $id1")
+
+        val noteItem2 = NoteItem(title = "Заметка 2", content = "Содержимое заметки 2")
+        note.add(noteItem2)
+        val id2 = note.getId()
+        assertEquals(2, id2)
+        println("ID второй заметки: $id2")
+
+        val noteItem3 = NoteItem(title = "Заметка 3", content = "Содержимое заметки 3")
+        note.add(noteItem3)
+        val id3 = note.getId()
+        assertEquals(3, id3)
+        println("ID третьей заметки: $id3")
+    }
 
     @Test
     fun testDeleteComment() {
